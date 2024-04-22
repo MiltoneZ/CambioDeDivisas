0fd8910769265894ab35a96040ff235321689266
MiltoneZ <milton.uzieldm@gmail.com>
2024-04-22 13:38:13 -0600

    Primer caso

--- src/Formato.java
+++ src/Formato.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Formato {
     public static void main(String[] args) {
         System.out.println(Textos.topBar);
         System.out.println(Textos.menu);
+
+        System.out.println("Digite el tipo que operacion que desea realizar: ");
+        Scanner teclado = new Scanner(System.in);
+        int opcion = Integer.parseInt(teclado.nextLine());
+
+        switch (opcion){
+            case 1:
+                System.out.println("Numero 1");
+        }
     }
 }
