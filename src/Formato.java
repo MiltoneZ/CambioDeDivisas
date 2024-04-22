80db5fe35a33ab28dd6bb132c4840c880f50c290
MiltoneZ <milton.uzieldm@gmail.com>
2024-04-22 13:23:08 -0600

    Menu del cambio de moneda.

--- src/Formato.java
+++ src/Formato.java
@@ -2,10 +2,5 @@ public class Formato {
     public static void main(String[] args) {
         System.out.println(Textos.topBar);
         System.out.println(Textos.menu);
-        switch (true){
-
-
-
-        }
     }
 }
